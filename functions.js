5dd4fb3a25ea6c17b85d613a82de4679b198fcb6
Ivy-Wanjiku <wanjiruwanjikuivy@gmail.com>
2024-03-29 11:41:46 +0300

    push changes

---
+++ functions.js
@@ -0,0 +1,23 @@
+let array=["Happy","Hope","Cool","dry"]
+function arrSort(array){
+    array.sort();
+    return array;
+}
+console.log(arrSort(array));
+function reverse (array){
+array.reverse(array)
+return array
+}
+console.log(reverse(array));
+let num=[1,2 -5,-7,0,8,3,-9]
+function returnPositive(num){
+if (num =>0) {
+   pos_count++ 
+   console.log(true); 
+}
+else{
+    console.log(false)
+}
+return num
+}
+console.log(num);
